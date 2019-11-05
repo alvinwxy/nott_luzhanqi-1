44af24515a54f8e041878be965e4f595dc79e8bf
alvinwxy <57216423+alvinwxy@users.noreply.github.com>
2019-11-05 12:32:02 +0800

    changed button class to draw transparent buttons

--- src/button.py
+++ src/button.py
@@ -8,18 +8,20 @@ class Button():
         self.width = width
         self.height = height
         self.text = text
-
+        self.button_surface = pygame.Surface((self.width,self.height), pygame.SRCALPHA)   # per-pixel alpha
 
     def draw(self,win,outline=None):
         #Call this method to draw the button on the screen
         if outline:
             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
             
-        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        self.button_surface.fill(self.getColor())
+        win.blit(self.button_surface, (self.x, self.y))
         
         if self.text != '':
             font = pygame.font.SysFont('comicsans', 12)
             text = font.render(self.text, 1, (0,0,0))
+            #Position the text on the center of the button
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self, pos):
@@ -33,7 +35,7 @@ class Button():
     def isPressed(self, pos):
         #Pos is the mouse position or a tuple of (x,y) coordinates
         mouseClick = pygame.mouse.get_pressed()
-        if mouseClick[0] == True and Button.isOver(self,pos):
+        if mouseClick[0] == True and self.isOver(pos):
             return True
 
         return False
@@ -47,4 +49,4 @@ class Button():
         self.color = color
 
     def update(self,color):
-        Button.setColor(self,color)
+        self.setColor(color)
