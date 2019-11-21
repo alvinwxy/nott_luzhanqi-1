222068b8658fddd44f20d15fe01319507ddf0aa7
alvinwxy <57216423+alvinwxy@users.noreply.github.com>
2019-11-22 00:38:37 +0800

    Board is now responsible to draw both the board tiles & selection pane

--- src/board.py
+++ src/board.py
@@ -1,105 +1,175 @@
 import pygame
 import operator
 from button import Button
+from pieces import *
 
 class Board:
     def __init__(self,width,height,numRow,numCol):
-        self.red = pygame.Color(200,50,50)
-        self.green = pygame.Color(50,200,50)
-        self.yellow = pygame.Color(200,200,50)
-        self.blue = pygame.Color(50,50,200)
-        self.purple = pygame.Color(200,50,200)
+        self.red = pygame.Color(255,0,0)
+        self.green = pygame.Color(0,255,0)
+        self.blue = pygame.Color(0,0,255)
+        self.black = pygame.Color(0,0,0)
         self.width = width
         self.height = height
         self.numRow = numRow
         self.numCol = numCol
-        self.board = self.initialise()
-        self.button = self.initialiseButton()
+        self.layout = self.generateLayout()
+        #record all tile status
+        self.tiles = self.generateTiles()
+        #record all selection pane status
+        self.selectionPaneTiles = self.generateSelectionPane()
+        self.pieceData = {"Flag": [1],
+                          "Grenade": [2],
+                          "Landmine": [3],
+                          "Marshal": [1],
+                          "General": [1], 
+                          "Lieutenant": [2], 
+                          "Brigadier": [2],
+                          "Colonel": [2], 
+                          "Major": [2], 
+                          "Captain": [3], 
+                          "Commander": [3],
+                          "Engineer": [3]}
 
-    def initialise(self):
-        """Initialise the game board"""
+
+    def generateLayout(self):
+        #Initialise the game board
         #Set all as Soldier Station
-        board = [["SS" for j in range(self.numCol)] for i in range(self.numRow)]
+        layout = [["SS" for i in range(self.numCol)] for j in range(self.numRow)]
 
         #Setting Camp
-        board[2][1] = "CP"
-        board[2][3] = "CP"
-        board[3][2] = "CP"
-        board[4][1] = "CP"
-        board[4][3] = "CP"
-        board[8][1] = "CP"
-        board[8][3] = "CP"
-        board[9][2] = "CP"
-        board[10][1] = "CP"
-        board[10][3] = "CP"
+        layout[2][1] = "CP"
+        layout[2][3] = "CP"
+        layout[3][2] = "CP"
+        layout[4][1] = "CP"
+        layout[4][3] = "CP"
+        layout[8][1] = "CP"
+        layout[8][3] = "CP"
+        layout[9][2] = "CP"
+        layout[10][1] = "CP"
+        layout[10][3] = "CP"
 
         #Setting Headquarters
-        board[0][1] = "HQ"
-        board[0][3] = "HQ"
-        board[12][1] = "HQ"
-        board[12][3] = "HQ"
+        layout[0][1] = "HQ"
+        layout[0][3] = "HQ"
+        layout[12][1] = "HQ"
+        layout[12][3] = "HQ"
 
         #Setting Front Line
-        board[6][0] = "FL"
-        board[6][2] = "FL"
-        board[6][4] = "FL"
+        layout[6][0] = "FL"
+        layout[6][2] = "FL"
+        layout[6][4] = "FL"
 
         #Setting Mountain Border
-        board[6][1] = "MB"
-        board[6][3] = "MB"
+        layout[6][1] = "MB"
+        layout[6][3] = "MB"
 
-        return board
+        return layout
 
-    def initialiseButton(self):
-        color = (200,200,200)
-        button = [[Button(color,i*self.width,j*self.height,self.width,self.height,"placeholder") for i in range(self.numCol)] for j in range(self.numRow)]
+    def generateTiles(self):
+        tiles = [[Button(i * self.width, j * self.height, self.width, self.height, transparent = True, text = 'placeholder') for i in range(self.numCol)] for j in range(self.numRow)]
+        return tiles
 
-        for i in range(self.numRow):
-            for j in range(self.numCol):
-                color = Board.updateColor(self,self.board[i][j])
+    def generateSelectionPane(self):
+        selectionPaneTiles = [None for i in range(12)]
+        #draws text and pieces beside board
+        x = 725
+        y = 200
+        for i in range(12):
+            selectionPaneTiles[i] = Button(x, y, 50, 50, color = (255,255,0))
+            #better way?
+            if i == 0:
+                selectionPaneTiles[i].setPiece(Flag(0,selectionPaneTiles[i].getPos()))
+            elif i == 1:
+                selectionPaneTiles[i].setPiece(Grenade(0,selectionPaneTiles[i].getPos()))
+            elif i == 2:
+                selectionPaneTiles[i].setPiece(Landmine(0,selectionPaneTiles[i].getPos()))
+            elif i == 3:
+                selectionPaneTiles[i].setPiece(Marshal(0,selectionPaneTiles[i].getPos()))
+            elif i == 4:
+                selectionPaneTiles[i].setPiece(General(0,selectionPaneTiles[i].getPos()))
+            elif i == 5:
+                selectionPaneTiles[i].setPiece(Lieutenant(0,selectionPaneTiles[i].getPos()))
+            elif i == 6:
+                selectionPaneTiles[i].setPiece(Brigadier(0,selectionPaneTiles[i].getPos()))
+            elif i == 7:
+                selectionPaneTiles[i].setPiece(Colonel(0,selectionPaneTiles[i].getPos()))
+            elif i == 8:
+                selectionPaneTiles[i].setPiece(Major(0,selectionPaneTiles[i].getPos()))
+            elif i == 9:
+                selectionPaneTiles[i].setPiece(Captain(0,selectionPaneTiles[i].getPos()))
+            elif i == 10:
+                selectionPaneTiles[i].setPiece(Commander(0,selectionPaneTiles[i].getPos()))
+            elif i == 11:
+                selectionPaneTiles[i].setPiece(Engineer(0,selectionPaneTiles[i].getPos()))
 
-                button[i][j].setColor(color)
+            if x > 1050:
+                x = 725
+                y += 150
+                #go to new line
+            else:
+                x += 120
 
-        return button
-
+        return selectionPaneTiles
 
     def draw(self,surface):
-        """Draw the Board"""
+        #Draw Board
         for j in range(self.numCol):
             for i in range(self.numRow):
-                self.button[i][j].draw(surface,2)
+                self.tiles[i][j].draw(surface)
 
-    def updateColor(self,gridType):
-        if gridType == "SS":
-            color = self.red
-        elif gridType == "CP":
-            color = self.green
-        elif gridType == "HQ":
-            color = self.yellow
-        elif gridType == "FL":
-            color = self.blue
-        elif gridType == "MB":
-            color = self.purple
+        #Only draw Selection Pane on setup phase
+        #Draw Selection Pane
+        #Draw Selection Pane Title
+        titleTextObj = pygame.font.Font("bin\OpenSans.ttf", 38)
+        titleTextSurfaceObj = titleTextObj.render("PIECES", True, self.black)
+        titleTextRectObj = titleTextSurfaceObj.get_rect()
+        titleTextRectObj.center = (925, 75)
+        surface.blit(titleTextSurfaceObj, titleTextRectObj)
+        #Draw Selection Pane Tiles
+        for k in range(len(self.selectionPaneTiles)):
+            self.selectionPaneTiles[k].draw(surface)
+            #Draw Selection Pane piece's name
+            textObj = pygame.font.Font("bin\OpenSans.ttf", 18)
+            textSurfaceObj = textObj.render(self.selectionPaneTiles[k].getPiece().toString(), True, self.black)
+            textRectObj = textSurfaceObj.get_rect()
+            textRectObj.center = tuple(x + y for x, y in zip(self.selectionPaneTiles[k].getPos(), (25,-25)))
+            surface.blit(textSurfaceObj, textRectObj)
 
-        return color
+            numTextSurfaceObj = textObj.render("x " + str(self.selectionPaneTiles[k].getPiece().getAvailable()), True, self.black)
+            numTextRectObj = numTextSurfaceObj.get_rect()
+            numTextRectObj.center = tuple(x + y for x, y in zip(self.selectionPaneTiles[k].getPos(), (25,75)))
+            surface.blit(numTextSurfaceObj, numTextRectObj)
 
-    def update(self,pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
-        for i in range(self.numRow):
-            for j in range(self.numCol):
-                if self.button[i][j].isPressed(pos):
-                    #make button darker
-                    color = tuple(x + y for x, y in zip(self.button[i][j].getColor(), (-100,-100,-100)))
-                elif self.button[i][j].isOver(pos):
-                    #make button brighter
-                    color = tuple(x + y for x, y in zip(self.button[i][j].getColor(), (50,50,50)))
-                else:
-                    color = Board.updateColor(self,self.board[i][j])
+    def handleEvent(self, event):
+        #call button event handler
+        for j in range(self.numCol):
+            for i in range(self.numRow):
+                outline = False
+                outlineColor = None
+                if 'hover' in self.tiles[i][j].handleEvent(event):
+                    #if is hovering on button
+                    outline = True
+                    outlineColor = self.green
+                if 'click' in self.tiles[i][j].handleEvent(event):
+                    #if button is clicked
+                    outline = True
+                    outlineColor = self.blue
+                if 'exit' in self.tiles[i][j].handleEvent(event):
+                    #if mouse exited a button
+                    outline = False
+                self.tiles[i][j].update(self.tiles[i][j].getColor(), outline, outlineColor)
 
-                invalidColor = False
-                for k in range(3):
-                    if color[k] > 255 or color[k] < 0:
-                        invalidColor = True
-                
-                if not invalidColor:
-                    self.button[i][j].update(color)
+        for k in range(len(self.selectionPaneTiles)):
+            outline = False
+            outlineColor = None
+            if 'hover' in self.selectionPaneTiles[k].handleEvent(event):
+                #if is hovering on button
+                outline = True
+                outlineColor = self.red
+            if 'click' in self.selectionPaneTiles[k].handleEvent(event):
+                outline = True
+                outlineColor = self.black
+            if 'exit' in self.selectionPaneTiles[k].handleEvent(event):
+                outline = False
+            self.selectionPaneTiles[k].update(self.selectionPaneTiles[k].getColor(), outline, outlineColor)
--- src/game.py
+++ src/game.py
@@ -1,130 +1,43 @@
 import pygame
 from board import Board
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+yellow = (255,255,0)
+black = (0,0,0)
+white = (255,255,255)
+
+FPS = 30
+displayWidth = 1200
+displayHeight = 716   #1 extra pixel to see the last line
+
 pygame.init()
-
-red = (255, 0, 0)
-blue = (0, 0, 255)
-white = (255, 255, 255)
-yellow = (247, 255, 0)
-black = (0, 0, 0)
-
-mouseX = 0
-mouseY = 0
-
-displayX = 1200
-displayY = 716   #1 extra pixel to see the last line
-
+FPSCLOCK = pygame.time.Clock()
+GAMEDISPLAY = pygame.display.set_mode((displayWidth, displayHeight))
+pygame.display.set_caption("Lu Zhan QI")
+#initialise board
 board = Board(100,55,13,5)
 
-win=pygame.display.set_mode((700,700))
-
-gameDisplay=pygame.display.set_mode((displayX,displayY))
-pygame.display.set_caption("Testing Codes")
-gameDisplay.fill(white)
-
-pieceData = {"Field Marshal": [1], "General": [1], "Lieutenant General": [2], "Brigadier": [2], "Colonel": [2], "Major": [2], "Captain": [3], "Platoon Commander": [3], "Engineer": [3], "Landmine": [3], "Bomb": [2], "Flag": [1]}
-
-rankData = {"Field Marshal": 1, "General": 2, "Lieutenant General": 3, "Brigadier": 4, "Colonel": 5, "Major": 6, "Captain": 7, "Platoon Commander": 8, "Engineer": 9, "Landmine": 0, "Bomb": 0, "Flag": "F"}
-
-def genPieceObject(x, y, item, color, showEnemy, drawShadow):
-
-    if drawShadow == True:
-
-        shadowImage = pygame.image.load("bin\Piece Shadow.png")
-        gameDisplay.blit(shadowImage, (x + 2, y + 2))
-
-    pieceRect = pygame.Rect(x, y, 50, 50)  #50 by 50 because its the size of the icons
-    pygame.draw.rect(gameDisplay, color, pieceRect)
-
-    if color != blue or showEnemy == True or alwaysShowEnemy == True:
+rankData = {"Field Marshal": 1, "General": 2, "Lieutenant General": 3, "Brigadier": 4, "Colonel": 5, "Major": 6, "Captain": 7, "Platoon Commander": 8, "Engineer": 9, "Landmine": 0, "Grenade": 0, "Flag": "F"}       
             
-        image = pygame.image.load("bin\\" + item + ".png")
-        gameDisplay.blit(image, (x, y))
-        
-
-    return pieceRect
-
-
-def setupPhase():
-
-    global pieceData, ready
-
-    gameDisplay.fill(white)
-
-    #draws text and pieces beside board
-    titleTextObj = pygame.font.Font("bin\OpenSans.ttf", 38)
-    titleTextSurfaceObj = titleTextObj.render("PIECES", True, black)
-    titleTextRectObj = titleTextSurfaceObj.get_rect()
-    titleTextRectObj.center = (925, 75)
-    gameDisplay.blit(titleTextSurfaceObj, titleTextRectObj)
-
-    textObj = pygame.font.Font("bin\OpenSans.ttf", 18)
-
-    x = 745
-    y = 150
-
-    for item in pieceData:
-        #drawing pieces icon
-
-        textSurfaceObj = textObj.render(item, True, black)
-        textRectObj = textSurfaceObj.get_rect()
-        textRectObj.center = (x, y)
-        gameDisplay.blit(textSurfaceObj, textRectObj)
-
-        numTextSurfaceObj = textObj.render("x" + str(pieceData[item][0]), True, black)
-        numTextRectObj = numTextSurfaceObj.get_rect()
-        numTextRectObj.center = (x, y + 100)
-        gameDisplay.blit(numTextSurfaceObj, numTextRectObj)
-
-        if pieceData[item][0] != 0:
-
-            ready = False
-            pieceRect = genPieceObject(x - 25, y + 25, item, yellow, False, False)
-            pieceData[item] = [pieceData[item][0], pieceRect]
-
-
-        if x > 1050:
-
-            x = 745
-            y += 150
-
-        else:
-            x += 120
-            
-    #drawing done icon
-
-    buttonRect = pygame.Rect(displayX - 115, displayY - 55, 100, 40)
-    pygame.draw.rect(gameDisplay, red, buttonRect)
-
-    endPhaseTextSurfaceObj = textObj.render("DONE", True, black)
-    endPhaseTextRectObj = endPhaseTextSurfaceObj.get_rect()
-    endPhaseTextRectObj.center = (buttonRect.left + 50, buttonRect.top + 20)
-    gameDisplay.blit(endPhaseTextSurfaceObj, endPhaseTextRectObj)
-
-    if buttonRect.left < mouseX < buttonRect.right and buttonRect.top < mouseY < buttonRect.bottom and mouseClicked == True:
-        pieceData = {"Field Marshal": 1, "General": 1, "Lieutenant General": 2, "Brigadier": 2, "Colonel": 2, "Major": 2, "Captain": 3, "Platoon Commander": 3, "Engineer": 3, "Landmine": 3, "Bomb": 2, "Flag": 1}
-        gameDisplay.fill(white)
-         
-            
-
-def drawBoard(board): 
-    board.draw(gameDisplay)
+def draw(board):
+    GAMEDISPLAY.fill(white)
+    board.draw(GAMEDISPLAY)
     
-run=True
-
-setupPhase()
+run = True
 
 while run:
     pygame.time.Clock()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run=False
+            run = False
+        board.handleEvent(event)
 
-    board.update(pygame.mouse.get_pos())
-
-    drawBoard(board)
+    draw(board)
 
     pygame.display.update()
+    FPSCLOCK.tick(FPS)
 
 pygame.quit()
